81d35c91c6d57bf7880668e5cd5b881e63f0849d
mpotts0201 <mpotts0201@gmail.com>
2019-01-14 16:43:08 -0500

    Trying different path in static.json

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :api do 
+    root 'recipes#landing'
+    get '*path', to: 'recipes#landing'
     get '/recipes', to: 'recipes#search'
   end
 
-  root 'recipes#landing'
-  get '*path', to: 'recipes#landing'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
