e5e974098c1827fe70785725fdc7811b7ba88a62
mpotts0201 <mpotts0201@gmail.com>
2019-01-14 17:07:17 -0500

    Trying fallback method in rails for 404s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  
+  def fallback_index_html
+    render file: 'public/index.html'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :api do 
-    root 'recipes#landing'
-    get '*path', to: 'recipes#landing'
     get '/recipes', to: 'recipes#search'
   end
 
 
+
+  # Gets rid of 404's on heroku deployment 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
